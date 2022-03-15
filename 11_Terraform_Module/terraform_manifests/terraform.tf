dc1b6731657a0640d9a8a3d11f93fcaa9b16a99b
devopsmela <devopsmela@gmail.com>
2022-03-15 19:38:25 +0530

    Added Module

---
+++ 11_Terraform_Module/terraform_manifests/terraform.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.97.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+module "rg" {
+  source  = "claranet/rg/azurerm"
+  version = "5.0.1"
+  # insert the 4 required variables here
+  client_name = "DevOps Mela"
+  environment = "Production"
+  location = "eastus"
+  stack = "DevOps Project"
+}
+
+
+
+# module "rg" {
+#   source  = "claranet/rg/azurerm"
+#   version = "5.0.1"
+#   # insert the 4 required variables here
+#   client_name = "DevOpsMela"
+#   environment = "development"
+#   location = "eastus"
+#   stack = "dev_stack"
+# }
